215c8133874b461029ff99a8300be29b8a392ca4
gabriella <gabriella.hedesand@hotmail.com>
2022-03-02 13:57:28 +0100

    commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001
 
 let users = {};
 app.use(function (req, res, next) {
-   //res.setHeader('Content-Security-Policy', "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'");
+   res.setHeader('Content-Security-Policy', "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'");
    next();
 });
 
